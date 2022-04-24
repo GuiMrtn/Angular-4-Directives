68b72fa3ba7c549f369396dbf6b249026cb4de40
Martin Piat <mrtn.piat@gmail.com>
2022-04-24 19:20:38 +0200

    directives

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BorderCardDirective } from './border-card.directive';
 
 @NgModule({
   declarations: [
-    AppComponent
+    //C'est ici qu'on déclare nos composants
+    AppComponent,
+    BorderCardDirective
   ],
   imports: [
+    //C'est ici qu'on importe les modules qui seront utilisés dans notre projet
     BrowserModule,
     AppRoutingModule
   ],
